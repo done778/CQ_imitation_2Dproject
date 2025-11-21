a26205c0b3534ba921f80df2770be015a42ccdf8
done778 <57587831+done778@users.noreply.github.com>
2025-11-21 10:52:03 +0900

    [feat] Generic Singletone Class
    싱글톤 제네릭 클래스 추가. 기본적인 게임매니저, 오디오매니저, UI매니저 싱글톤으로 적용

---
+++ Assets/2_Scripts/Managers/SingletonePattern.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SingletonePattern <T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            //인스턴스가 null이면 먼저 씬에서 찾아봄
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                // 그래도 없으면 새로 생성
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null) 
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
